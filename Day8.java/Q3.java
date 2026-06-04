b0d91296c57c0bbceb82906353b775dc6f63f076
jasKaurdhaliwal15 <jaskaurdhaliwal@15gmail.com>
2026-06-04 19:20:31 +0530

    Day8

---
+++ Day8.java/Q3.java
@@ -0,0 +1,13 @@
+public class Q3 {
+    public static void main(String[] args) {
+        for(int i = 1 ; i <= 5 ; i++){
+            for(int j = 1 ; j <= i ; j++){
+                char alpha = (char)(64 + j);
+                System.out.print(alpha);
+            }
+            System.out.println(" ");
+        }
+    }
+}
+
+
